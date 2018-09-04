4489f213410f1bc9a3947aab4d96771045ff87be
Enrique Izel <eleaoizel@gmail.com>
2018-09-04 14:22:18 -0400

    eeeeeiiiiii

--- aulaAed/inserir_novo.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-void inserir_novo (O_HEAP *vetor, int vetor)
-{
-        vetor->no_elem++;
-        vetor->chave[vetor->no_elem-1] = valor;
-        heapify_up(vetor, vetor->no_elem);
-}
